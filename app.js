52a385e01dfdc7f15f87f113c4be9de825cca168
ron <rinbar70@gmail.com>
2020-11-08 20:22:22 +0200

    Implement limit middleware.

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var express = require('express');
+var cookieParser = require('cookie-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const limit = function ({ max, time }) {
+    let reqs = 0, blocked = false;
+    return function (req, res, next) {
+        if (blocked) {
+            return res.sendStatus(500);
+        }
+        else if (++reqs < max) {
+            return next();
+        } else {
+            reqs = 0;
+            blocked = true;
+            setTimeout(function () {
+                blocked = false;
+            }, 10000 * time);
+            return res.sendStatus(500);
+        }
+    };
+}
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(limit({
+    max: 5, // amount
+    time: 1 // minout
+}));
+
+app.get('/test', (req, res) => {
+    res.json({ status: '(:' });
+});
 
 module.exports = app;
