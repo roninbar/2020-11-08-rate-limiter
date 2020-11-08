4a97248864493ea6c1d2b37713f6e7cf2c9d40fd
ron <rinbar70@gmail.com>
2020-11-08 20:39:19 +0200

    Add debug messages.

--- routers/limit.js
+++ routers/limit.js
@@ -1,16 +1,22 @@
+const debug = require('debug')('server:limit');
+
 module.exports = ({ max, time }) => {
     let reqs = 0, blocked = false;
     return (req, res, next) => {
         if (blocked) {
+            debug('Blocking request.');
             return res.sendStatus(500);
         }
         else if (++reqs < max) {
+            debug('Passing request.');
             return next();
         } else {
             reqs = 0;
             blocked = true;
+            debug('Starting to block requests.');
             setTimeout(function () {
                 blocked = false;
+                debug('Starting to pass requests.');
             }, 10000 * time);
             return res.sendStatus(500);
         }
